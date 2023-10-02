7201a741ac1a61be5e0a5ac9ed1ea73ceaeca9d4
Apasia45 <alexepasia@gmail.com>
2023-10-02 17:19:14 -0500

    UpdateCapstone

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,15 @@
 import html from "html-literal";
 
-export default (state) => html`
-
+export default (links) => html`
+   <nav>
+    <i></i>
+    <ul>
+      ${links
+        .map(
+          (link) =>
+            `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
+        )
+        .join("")}
+    </ul>
+  </nav>
 `;
--- index.js
+++ index.js
@@ -12,13 +12,28 @@ const router = new Navigo("/");
       ${Main(state)}
       ${Footer()}
     `;
-    afterRender();
+    // afterRender();
     router.updatePageLinks();
   }
 
-function afterRender() {
-// add menu toggle to bars icon in nav bar
-  document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-  });
-};
+// function afterRender() {
+// // add menu toggle to bars icon in nav bar
+//   document.querySelector(".fa-bars").addEventListener("click", () => {
+//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+//   });
+// };
+
+router
+.on({
+  "/": () => render(),
+  ":view": (params) => {
+    let view = capitalize(params.data.view);
+    if (view in store) {
+      render(store[view]);
+    } else {
+      render(store.Viewnotfound);
+      console.log(`View ${view} not defined`);
+    }
+  },
+})
+.resolve();
