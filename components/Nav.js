3cd97770b0a41aa243f44fb57e97860da3ff3eef
Apasia45 <alexepasia@gmail.com>
2023-10-09 13:42:03 -0500

    Still fixing CSS

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,3 @@
 import html from "html-literal";
 
-export default links => html`
-  <!-- <nav>
-    <i></i>
-    <ul>
-      ${links
-    .map(
-      link =>
-        `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
-    )
-    .join("")}
-    </ul>
-  </nav> -->
-`;
+export default links => html``;
