528efd3c3f4c73c4772e146626e2e3d25f97a0dd
Apasia45 <alexepasia@gmail.com>
2023-10-09 21:18:48 -0500

    Temp weather API

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,11 +1,16 @@
 import html from "html-literal";
 
-export default () => html`
+export default state => html`
   <section id="home">
     <h1>Eatr</h1>
     <a class="cont" href="Preferences" data-navigo>
       Continue to preferences
     </a>
     <a id="contactme" href="Contact" data-navigo>Contact me here</a>
+    <h3>
+      The weather in ${state.weather.city} is ${state.weather.description}.
+      Temperature is ${state.weather.temp}F, and it feels like
+      ${state.weather.feelsLike}F.
+    </h3>
   </section>
 `;
