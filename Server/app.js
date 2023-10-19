2525872c45d005c10ecf56ea1479616decd1e730
Apasia45 <alexepasia@gmail.com>
2023-10-18 19:11:59 -0500

    Server Fix

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,38 @@
 // 'Import' the Express module instead of http
 import express from "express";
+import dotenv from "dotenv";
 // Initialize the Express application
 const app = express();
+// Load environment variables from .env file
+dotenv.config();
+
+const PORT = process.env.PORT || 4040;
+
+const logging = (request, response, next) => {
+  console.log(
+    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
+  );
+  next();
+};
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
@@ -11,13 +42,8 @@ app.get("/status", (request, response) => {
   response.send(JSON.stringify({ message: "Service healthy" }));
 });
 
+app.use(); // use the myMiddleware for every request to the app
+
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-app.listen(4040, () => console.log("Listening on port 4040"));
-
-const myMiddleware = (request, response, next) => {
-  // do something with request and/or response
-  next(); // tell express to move to the next middleware function
-};
-
-app.use(myMiddleware); // use the myMiddleware for every request to the app
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
