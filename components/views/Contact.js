ddf4259d53a389bbbe8893ac34fef08466b66f64
Apasia45 <alexepasia@gmail.com>
2023-11-02 13:28:47 -0500

    push

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -3,7 +3,7 @@ import html from "html-literal";
 export default () => html`
   <section id="contactBackGround">
     <a id="BTH" href="Home" data-navigo>Back to home</a>
-    <h3>Reach me here alexepasia@gmail.com test</h3>
+    <h3>Reach me here alexepasia@gmail.com</h3>
   </section>
 `;
 //just a floating box with that text with a back to home button in the top left.
